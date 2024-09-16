cbd8b4494e37e79213aa31d22a79d1920065b17d
mondrita_roy <mondrita.roy@catalyticsdatum.com>
2024-09-16 18:47:32 +0530

    Tesco_Interview_DataCompare

--- src/main/scala/Spark/CompareToDF.scala
+++ src/main/scala/Spark/CompareToDF.scala
@@ -14,6 +14,17 @@ val df1 =  spark.read.option("header", true).csv("C:\\Users\\Admin\\IdeaProjects
 
 val df2 =   spark.read.option("header", true).csv("C:\\Users\\Admin\\IdeaProjects\\Spark_Project\\src\\main\\resources\\CustomerDF2")
 
+  //One way but it gives only dataset from 1 dataframe
   df1.except(df2).show()
+ //Another Way to get all the data difference from both the table
+  df1.createTempView("table1")
+  df2.createTempView("table2")
+
+ val getdatafromdf1 = spark.sql("select * from table1 minus select * from table2")
+
+ val getdatafromdf2 =   spark.sql("select * from table2 minus select * from table1")
+
+ println( getdatafromdf1.unionAll(getdatafromdf2).count)
+
 
 }
